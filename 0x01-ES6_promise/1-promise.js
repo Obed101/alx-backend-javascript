313fc158e3415dc80da4b8fbfbd3a74de1b66f90
Obed101 <obedamoako92@gmail.com>
2022-04-05 20:41:32 +0000

    fixed resolve issue

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,14 @@
 export function handleResponseFromAPI (promise) {
-  if (promise) {
-    const workDone = {
-      status: 200,
-      body: 'Success'
-    };
-    return Promise.resolve(workDone);
-  } else {
-    const failed = 'The fake API is not working currently';
-    return Promise.reject(failed);
-  }
+  return new Promise((resolve, reject) => {
+	  if (promise) {
+      const workDone = {
+        status: 200,
+        body: 'Success'
+      };
+      return Promise.resolve(workDone);
+    } else {
+      const failed = 'The fake API is not working currently';
+      return Promise.reject(failed);
+    }
+  });
 }
