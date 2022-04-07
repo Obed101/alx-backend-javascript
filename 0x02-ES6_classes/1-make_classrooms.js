95abc54535015dc0caa6dd165e068820f242ab7d
Obed101 <obedamoako92@gmail.com>
2022-04-07 20:24:18 +0000

    fixed issues with new instances

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,6 @@
 import classRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  const classroom1 = new classRoom(19);
-  const classroom2 = new classRoom(20);
-  const classroom3 = new classRoom(34);
-  const classes = [classroom1, classroom1, classroom1];
+  const classes = [new classroom(19), new classroom(20), new classroom(34)];
   return classes;
 }
