f73c50fb7aeb9a6da491db3edcdbd9a73316a958
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2022-04-07 22:47:08 +0000

    classroom import fixed

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,4 @@
-import classRoom from './0-classroom.js';
+import classRoom from './0-classroom';
 
 export default function initializeRooms() {
   const classes = [new classroom(19), new classroom(20), new classroom(34)];
