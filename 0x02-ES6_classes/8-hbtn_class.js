4e606040d489f1e2c809a660e3b088951ca8a8aa
Obed101 <obedamoako92@gmail.com>
2022-04-09 14:48:26 +0000

    override every method

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,8 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+  Number(this) = this._size;
+  String(this) = this._location;
+}
