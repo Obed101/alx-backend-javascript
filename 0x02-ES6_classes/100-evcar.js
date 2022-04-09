39c4f1dba3d49eaa1e3acc2260bd046c2b1275f2
Obed101 <obedamoako92@gmail.com>
2022-04-09 15:19:56 +0000

    modified the symbol file

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -4,9 +4,9 @@ export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
-  }
 
-  static get [Symbol.species]() {
-    return Car;
+    static get [Symbol.species]() {
+      return Car;
+    }
   }
 }
