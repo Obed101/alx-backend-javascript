7382a368f88d09e8821f9a519a4483d1ad91d24b
Obed101 <obedamoako92@gmail.com>
2022-04-09 16:07:51 +0000

    fixed lint issue

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,6 @@
-export default function taskBlock (trueOrFalse) {
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
-  trueOrFalse = true;
 
   if (trueOrFalse) {
     const task = true;
