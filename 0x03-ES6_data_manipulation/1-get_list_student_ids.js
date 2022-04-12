831ea95b3dfdc57898a14e3e8e8316ac1200806e
Obed Amoako <obedamoako92@gmail.com>
2022-04-12 16:56:24 +0000

    added student id search

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,12 +1,13 @@
-export default function getListStudentIds(studends) {
+export default function getListStudentIds (studends) {
   let ids = [];
   if (Array.isArray(studends)) {
     for (let student of studends) {
-      if (student.includes("id")) {
-        ids.push(student.id);
+      if (student.includes('id')) {
+        const newmap = student.map(student.id);
+        ids.push(newmap);
       }
     }
     return ids;
   }
   return [];
-}
+};
