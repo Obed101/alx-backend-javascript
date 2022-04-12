b6212d61fc741896ce677f617a473c44e9a01fbe
Obed Amoako <obedamoako92@gmail.com>
2022-04-12 20:33:14 +0000

    added combination of array properties

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,14 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const std = students.filter(student => student.location === city);
+  std.map(student => {
+    let iniGrade = "N/A";
+    newGrades.forEach(grade => {
+      if (newGrades.grade) {
+        if (grade.studentId === student.id) {
+          iniGrade = grade.grade;
+        }
+      }
+      return { ...student, grade: iniGrade };
+    });
+  });
+}
