80847bc0c82fc3eca8712c262878c3dbc02c6d3e
Obed Amoako <obedamoako92@gmail.com>
2022-04-12 17:00:43 +0000

    simplified map function

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,6 @@
 export default function getListStudentIds (studends) {
-  let ids = [];
   if (Array.isArray(studends)) {
-    for (let student of studends) {
-      if (student.includes('id')) {
-        const newmap = student.map(student.id);
-        ids.push(newmap);
-      }
-    }
-    return ids;
+    return students.map((student) => student.id);
   }
   return [];
 };
