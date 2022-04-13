801e9580a55a763172fbf1689cc30a61da0f1998
Obed Amoako <obedamoako92@gmail.com>
2022-04-13 14:33:45 +0000

    class constructors and interfaces and functions

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,13 +6,35 @@ interface Teacher {
   location: string;
   [index: number]: boolean;
 }
+
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
 function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
 }
+
 interface printTeacher {
   firstName: string;
   lastName: string;
+}
+
+///// CLASS INITIALISATION /////
+class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+      return this.firstName;
+  }
 }
