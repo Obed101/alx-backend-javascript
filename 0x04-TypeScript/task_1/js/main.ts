bf1c236fceb3e822ad320c075ffb7778bb799492
Obed Amoako <obedamoako92@gmail.com>
2022-04-13 13:59:42 +0000

    added a function that prints first firstName[letter]. lastName

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -9,3 +9,6 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;
 }
+function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName[0]}. ${lastName}`;
+}
