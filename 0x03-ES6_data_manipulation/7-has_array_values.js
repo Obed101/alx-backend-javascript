536d977dfd7d67535f1fd8dc97feefb11b1ca323
Obed Amoako <obedamoako92@gmail.com>
2022-04-13 08:19:59 +0000

    return value validated

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,8 @@
 export default function hasValuesFromArray(set, array) {
-  for (let i of array) {
+  for (const i of array) {
     if (!set.has(i)) {
       return false;
     }
-    return true;
   }
+  return true
 }
