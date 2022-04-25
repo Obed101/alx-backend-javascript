ef07973a4e215ff3f7418a6069a7bd97ef3d2a8b
Obed Amoako <obedamoako92@gmail.com>
2022-04-25 09:27:12 +0000

    fixed lint problems

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
-export default function cleanSet (set, start) {
+export default function cleanSet(set, start) {
   const another = [];
   if (start === '' || typeof start !== 'string') return '';
-  set.forEach(element => {
+  set.forEach((element) => {
     if (element.startsWith(start)) {
       const final = element.replace(start, '');
       another.push(final);
