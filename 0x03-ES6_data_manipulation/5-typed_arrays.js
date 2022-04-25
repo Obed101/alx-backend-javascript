ed1a8612865deefdc619a07cd8687e650ed1b2bd
Obed Amoako <obedamoako92@gmail.com>
2022-04-25 09:19:38 +0000

    fixed lint issues

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-    if (position < 0 || position >= length) {
-      throw Error('Position outside range');
-    }
-    const type1 = new ArrayBuffer(length);
-    const array8 = new Int8Array(type1, 0, length);
-    array8.set([value], position);
-    return new DataView(type1);
+  if (position < 0 || position >= length) {
+    throw Error("Position outside range");
   }
+  const type1 = new ArrayBuffer(length);
+  const array8 = new Int8Array(type1, 0, length);
+  array8.set([value], position);
+  return new DataView(type1);
+}
