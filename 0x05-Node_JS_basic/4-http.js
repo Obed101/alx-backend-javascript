ffd441c2d0163bc7140298d41b8a3859d9966027
Obed Amoako <obedamoako92@gmail.com>
2022-05-30 16:24:33 +0000

    exporting the app

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,5 +1,6 @@
-const http = require('http');
+const app = require('http');
 http.createServer(function (_, res) {
   res.write('Hello Holberton School!');
   res.end();
 }).listen(1245);
+module.exports = app;
