61c8f6bbe0fbbdb6ced3f61a2940cea4b81b06fc
Obed Amoako <obedamoako92@gmail.com>
2022-05-30 11:32:06 +0000

    added vaidation to user interruption

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,8 +8,8 @@ readline.question(`Welcome to Holberton School, what is your name?\n`, name => {
   console.log(`Your name is: ${name}`);
   readline.close();
 });
-const ac = new AbortController();
-const signal = ac.signal;
-signal.addEventListener('abort', () => {
+process.on('exit', () => {
   console.log('This important software is now closing');
-}, { once: true });
+});
+
+// process.on('exit')
