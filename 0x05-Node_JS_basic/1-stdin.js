2dddaacc7c5c574a9c0963de9056b3e43203babd
Obed101 <obedamoako92@gmail.com>
2022-05-30 11:30:51 +0000

    removed unused

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,8 +8,3 @@ readline.question(`Welcome to Holberton School, what is your name?\n`, name => {
   console.log(`Your name is: ${name}`);
   readline.close();
 });
-const ac = new AbortController();
-const signal = ac.signal;
-signal.addEventListener('abort', () => {
-  console.log('This important software is now closing');
-}, { once: true });
