790a4aa37a97a844c151826e6f9449e56bebc020
Obed Amoako <obedamoako92@gmail.com>
2022-05-31 11:24:58 +0000

    fixed server problem

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,6 +3,7 @@ const app = require('http');
 app.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+  res.write('Hello Holberton School!');
+  res.end();
 }).listen(1245);
 module.exports = app;
