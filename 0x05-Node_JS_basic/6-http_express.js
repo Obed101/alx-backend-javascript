76937043363c24f1805178e96b6fca46539fb2a0
Obed Amoako <obedamoako92@gmail.com>
2022-05-31 12:56:41 +0000

    changed export value

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-const app = express.get('/', (_, response) => {
+const app = express();
+app.get('/', (_, response) => {
   response.setHeader('Content-Type', 'text/plain');
   response.send('Hello Holberton School!');
 }).listen(1245);
