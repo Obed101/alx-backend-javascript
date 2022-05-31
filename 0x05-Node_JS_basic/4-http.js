adf835495f217c9408d8b80fdeb175e87e51515b
Obed Amoako <obedamoako92@gmail.com>
2022-05-31 11:27:54 +0000

    changed the export value

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,10 @@
-const app = require('http');
+let app = require('http');
 
-app.createServer((_, res) => {
+app = app.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.write('Hello Holberton School!');
   res.end();
 }).listen(1245);
+
 module.exports = app;
