255cce6e4102d1c87d0f9ed52578505ce26e51b0
Obed Amoako <obedamoako92@gmail.com>
2022-06-03 11:15:11 +0000

    completed mocha, changed the description

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -6,13 +6,13 @@ let expect = require('chai');
 expect = expect.expect;
 
 describe('Testing calculateNumber function with differemt types', function () {
-  it('Should return sum of two rounded numbers', () => {
+  it('Should test SUM and return 2', () => {
     expect(calcul('SUM', 1.2, 1.4)).to.equal(2);
   });
-  it('Should return subtraction of two rounded numbers', () => {
+  it('Should test SUBTRACT and return 14', () => {
     expect(calcul('SUBTRACT', 45.2, 30.9)).to.equal(14);
   });
-  it('Should return division of two rounded numbers', () => {
+  it('Should test DIVIDE and return 30', () => {
     expect(calcul('DIVIDE', 90.4, 3.3)).to.equal(30);
   });
   it('Should return an Error', () => {
