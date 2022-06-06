0ced8f9b1de799fd166de347aa349114053c5fc0
Obed Amoako <obedamoako92@gmail.com>
2022-06-06 08:15:46 +0000

    fixed api response body

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -7,7 +7,7 @@ app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
 });
 app.get('/cart/:id([0-9]+)', (req, res) => {
-  console.log(req.params.id)
+  console.log(req.params.id);
   res.send(`Payment methods for cart ${req.params.id}`);
 });
 app.get('/available_payments', (_, res) => {
@@ -16,12 +16,11 @@ app.get('/available_payments', (_, res) => {
       credit_cards: true,
       paypal: false
     }
-  }
+  };
   res.send(payments);
 });
-app.post('/login', (req, res) => {
-  const { userName } = req.body;
-  res.send(`Welcome ${userName}`);
+app.post('/login', function (request, response) {
+  response.send(`\nHello ${request.body}`);
 });
 
 app.listen(port, () => {
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const request = require('request');
+const fs = require('fs');
 
 describe('Testing API endpoints with different values', () => {
   it('should return the right response message', (done) => {
@@ -44,6 +45,17 @@ describe('Testing API endpoints with different values', () => {
     };
     request(options, (_, res) => {
       expect(res.body).to.include('payment_methods');
+      done();
+    });
+  });
+  it('Should return Welcome to the user', (done) => {
+    const data = { userName: 'Obed' };
+    request.post('http://localhost:7865/login', {
+      json: {
+        userName: 'Obed'
+      }
+    }, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
       done();
     });
   });
